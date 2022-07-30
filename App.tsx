d5a1f303cf3a3c314a9c6116a2aa5ede065cc3e2
TerminaD <1499558179@qq.com>
2022-07-30 15:55:04 +0800

    Implemented basic tab bar.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
 
-export default function App() {
+import LearnTab from './src/components/LearnTab';
+import SettingsTab from './src/components/SettingsTab';
+import SocializeTab from './src/components/SocializeTab';
+
+const App = () => {
+  // Create instance of BottomTabNavigator
+  const Tab = createBottomTabNavigator()
+
+  // Return main switcher screen
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="学习" component={LearnTab} />
+        <Tab.Screen name="社交" component={SocializeTab} />
+        <Tab.Screen name="设置" component={SettingsTab} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App
